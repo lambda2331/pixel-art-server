e1660e81e35b3caa172a17426af81b04b46b3cf1
Artsem Dudinskij <artsem.dudinskij@exposit.com>
2021-08-10 15:02:33 +0300

    update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require('cors')
 const { PixelIt } = require("./pixel-it");
 const app = express();
 let port = process.env.PORT || 7000;
@@ -7,19 +8,16 @@ let port = process.env.PORT || 7000;
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true})); 
 
-app.post("/generate-art", async (request, response) => {
+app.post("/generate-art", cors(), async (request, response) => {
   try {
     const { base64: from, palette, scale } = request.body
     const art = new PixelIt({ from, palette, scale })
     const pixerArt = art.pixelate()
-
-    response.setHeader('Access-Control-Allow-Origin', '*');
-    response.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');  
+    console.log(pixerArt)
     response.send({
       pixelArt: pixerArt
     })
   } catch (e) {
-    console.log(e)
     response.send(e);
   }
 });
